033b5c28a18976c5a3facb06f9e80fd4d9ee87ce
MortenGitKonto <morten.jogi@gmail.com>
2020-09-17 15:50:27 +0300

    Morteni front end - work order

---
+++ src/components/all-workOrders.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="list row">
+    <div class="col-md-6">
+      <h4>Work Order List</h4>
+      <ul class="list-group">
+        <li class="list-group-item" v-for="(workOrder, index) in workOrders" :key="index">
+          {{ workOrder.jobDescription }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import ServiceWorkOrder from "@/Services/ServiceWorkOrder";
+
+export default {
+  name: "all-workOrders",
+  data() {
+    return {
+      workOrders: [],
+      jobDescription: ""
+    };
+  },
+  methods: {
+    retrieveWorkOrders() {
+      ServiceWorkOrder.getAll()
+          .then(response => {
+            this.workOrders = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    }
+  },
+  mounted() {
+    this.retrieveWorkOrders();
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
