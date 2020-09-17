70bf62e1faf42e6251beb4983c630492cccd6bdc
jantilk <jaanustilk@gmail.com>
2020-09-17 15:26:01 +0300

    search clients

--- src/components/all-clients.vue
+++ src/components/all-clients.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="list row">
+    <div class="col-md-8">
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Search by name"
+               v-model="name"/>
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button"
+                  @click="searchName"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    </div>
+
     <div class="col-md-6">
       <h4>Clients List</h4>
       <ul class="list-group">
+        <li class="list-group-item" :class="{ active: index == currentIndex }" v-for="(client, index) in clients" :key="index"
+            @click="setActiveClient(client, index)">
         <li class="list-group-item" v-for="(client, index) in clients" :key="index">
           {{ client.name }}
         </li>
       </ul>
+    </div>
+
+    <div class="col-md-6">
+      <div v-if="currentClient">
+        <h4>Client</h4>
+        <div>
+          <label><strong>Name:</strong></label> {{ currentClient.name }}
+        </div>
+
+        <a class="badge badge-warning" :href="'/clients/' + currentClient.id">Edit</a>
+      </div>
+      <div v-else>
+        <br/>
+        <p>Please click on a Client...</p>
+      </div>
+
     </div>
   </div>
 </template>
@@ -20,12 +52,39 @@ export default {
   data() {
     return {
       clients: [],
+      currentClient: null,
+      currentIndex: -1,
       name: ""
     };
+    // return {
+    //   clients: [],
+    //   name: ""
+    // };
   },
   methods: {
     retrieveClients() {
       FrontDataService.getAll()
+          .then(response => {
+            this.clients = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    // refreshList() {
+    //   this.retrieveClients();
+    //   this.currentClient = null;
+    //   this.currentIndex = -1;
+    // },
+
+    setActiveClient(client, index) {
+      this.currentClient = client;
+      this.currentClient = index;
+    },
+    searchName() {
+      FrontDataService.findByName(this.name)
           .then(response => {
             this.clients = response.data;
             console.log(response.data);
