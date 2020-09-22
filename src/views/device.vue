131714bb6ca7f01759357ee7b174b7531ca7b684
jantilk <jaanustilk@gmail.com>
2020-09-22 15:42:38 +0300

    device filter, if empty shows all ++

--- src/views/device.vue
+++ src/views/device.vue
@@ -3,36 +3,36 @@
       <search-device></search-device>
     <div class="row">
     </div>
-<!--    <div class="submit-form">-->
-<!--      <div v-if="!submitted">-->
-<!--        <div class="form-group">-->
-<!--          <label for="client_id">Client ID</label>-->
-<!--          <input type="number" class="form-control" id="client_id" required v-model="device.clientId" name="ClientID"/>-->
-<!--        </div>-->
-<!--        <div class="form-group">-->
-<!--          <label for="sn">Serial Number</label>-->
-<!--          <input type="text" class="form-control" id="sn" required v-model="device.serialNumber" name="SerialNumber">-->
-<!--        </div>-->
-<!--        <div class="form-group">-->
-<!--          <label for="counter">Counter</label>-->
-<!--          <input type="number" class="form-control" id="counter" required v-model="device.counter" name="Counter">-->
-<!--        </div>-->
-<!--        <div class="form-group">-->
-<!--          <label for="product_id">Product ID</label>-->
-<!--          <input type="number" class="form-control" id="product_id" required v-model="device.productId" name="ProductID">-->
-<!--        </div>-->
+    <div class="submit-form">
+      <div v-if="!submitted">
+        <div class="form-group">
+          <label for="client_id">Client ID</label>
+          <input type="number" class="form-control" id="client_id" required v-model="device.clientId" name="ClientID"/>
+        </div>
+        <div class="form-group">
+          <label for="sn">Serial Number</label>
+          <input type="text" class="form-control" id="sn" required v-model="device.serialNumber" name="SerialNumber">
+        </div>
+        <div class="form-group">
+          <label for="counter">Counter</label>
+          <input type="number" class="form-control" id="counter" required v-model="device.counter" name="Counter">
+        </div>
+        <div class="form-group">
+          <label for="product_id">Product ID</label>
+          <input type="number" class="form-control" id="product_id" required v-model="device.productId" name="ProductID">
+        </div>
 
-<!--        <button @click="createDevice" class="btn btn-success">Create</button>-->
-<!--      </div>-->
+        <button @click="createDevice" class="btn btn-success">Create</button>
+      </div>
 
-<!--      <div v-else>-->
-<!--        <h4>Device created successfully!</h4>-->
-<!--        <button class="btn btn-success" @click="newDevice">Create another</button>-->
-<!--      </div>-->
-<!--      <br>-->
+      <div v-else>
+        <h4>Device created successfully!</h4>
+        <button class="btn btn-success" @click="newDevice">Create another</button>
+      </div>
+      <br>
 
-<!--      <search-device-any></search-device-any>-->
-<!--    </div>-->
+      <search-device-any></search-device-any>
+    </div>
   </div>
 </template>
 
