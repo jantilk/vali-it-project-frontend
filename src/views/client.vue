145bf83f136b73f7b5d0792ebf0b78a3b9d2032b
paadilaenutus <toomistu@gmail.com>
2020-09-28 15:08:48 +0300

    client & device page update

--- src/views/client.vue
+++ src/views/client.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
-    <search-client></search-client>
-    <div v-if="!submitted" class="row">
-      <div>
-        <input id="name" v-model="client.name" name="name" placeholder="create new client" required type="text"/>
+    <div class="row">
+      <div class="col-md-12">
+        <search-client></search-client>
       </div>
-      <button class="btn btn-success" @click="createClient">Create</button>
+    </div>
+    <div class="row" v-if="!submitted">
+      <div class="col-md-8">
+        <input v-model="client.name" class="form-control" name="name" placeholder="Create new client" required
+               type="text"/>
+      </div>
+      <div class="col-md-3" style="margin-left: 20px">
+        <button class="btn btn-success" @click="createClient">Create</button>
+      </div>
     </div>
     <div v-else>
       <h4>Client created successfully!</h4>
@@ -33,13 +40,14 @@ export default {
   methods: {
     createClient() {
       var data = {name: this.client.name};
-      if(this.client.name != null && this.client.name != ""){
-      ServiceClient.create(data)
-          .then(this.submitted = true)
-          .catch(e => {
-            console.log(e);
-          });
-    }},
+      if (this.client.name != null && this.client.name != "") {
+        ServiceClient.create(data)
+            .then(this.submitted = true)
+            .catch(e => {
+              console.log(e);
+            });
+      }
+    },
     newClient() {
       this.submitted = false;
       this.client = {};
